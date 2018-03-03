1a2b1a164bc8a4142c8c729fd96bb2897a581df3
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2018-03-03 16:07:08 +0900

    DataBinding対応 

--- app/src/main/java/com/example/yutarosuzuki/constraintlayoutsamples/MainActivity.kt
+++ app/src/main/java/com/example/yutarosuzuki/constraintlayoutsamples/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.yutarosuzuki.constraintlayoutsamples
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.yutarosuzuki.constraintlayoutsamples.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
